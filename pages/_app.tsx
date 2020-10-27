32d53081c1182df8b07dc29896fbf98688320858
Ward <WardMaes@users.noreply.github.com>
2020-10-27 22:04:30 +0100

    Remove unnecessary code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,14 @@
 import { NextComponentType } from 'next'
 import { AppContext, AppInitialProps, AppProps } from 'next/app'
-import { useMachine } from '@xstate/react'
 
 import '../styles/index.css'
-import { counterMachine } from '../lib/machines/counter'
-import { counterContext as CounterContext } from '../lib/context'
 
 const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
   Component,
   pageProps,
 }) => {
   const modifiedPageProps = { ...pageProps }
-
-  const machine = useMachine(counterMachine, { devTools: true })
-
-  return (
-    <CounterContext.Provider value={machine}>
-      <Component {...modifiedPageProps} />
-    </CounterContext.Provider>
-  )
+  return <Component {...modifiedPageProps} />
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,16 @@
 import Head from 'next/head'
-import { useContext } from 'react'
-
-import { counterContext } from '../lib/context'
 
 export default function Home() {
-  const [state, send] = useContext(counterContext)
-
-  const increase = () => {
-    send({ type: 'INCREASE' })
-  }
-
   return (
     <div className="container">
       <Head>
-        <title>Boilerplate</title>
+        <title>The Hive</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <div className="flex flex-col justify-center items-center">
-          <p>Count: {state.context.count}</p>
-          <button className="btn" onClick={increase}>
-            Increase
-          </button>
+          The Hive
         </div>
       </main>
     </div>
