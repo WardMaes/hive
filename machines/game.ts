ba388b5ca3a3266bb38f6189b6d1675beee10fa0
Toine <bestrandomnameever@gmail.com>
2020-10-30 01:04:13 +0100

    Alternate gameMachine

--- machines/game.ts
+++ machines/game.ts
@@ -75,3 +75,88 @@ export const gameMachine = Machine<GameContext, GameStateSchema, GameEvent>({
     },
   },
 })
+
+const turnMachine = Machine({
+  key: 'turn',
+  initial: '',
+  context: {
+    cellsPossibleDestinationsCurrentMove: [],
+  },
+  states: {
+    select: {
+      // Selecting either an already placed piece to move or a unplayed piece to place
+      entry: assign({
+        // Also includes logic which pieces are allowed to be selected
+        // - Won't break the hive
+        // - If it is the n-th (don't remember) move and the queen hasn't been placed
+        piecesAllowedToBePlaced: (context) => {
+          null
+        },
+        piecesAllowedToBeMoved: (context) => {
+          null
+        },
+      }),
+      on: {
+        // Dont know if it is possible to add identifier selected piece to the event but that would be the goal
+        // That or listener that links to function that sets the context
+        PLACE: 'place',
+        MOVE: 'move',
+      },
+    },
+    place: {
+      entry: assign({
+        cellsPossibleDestinationsCurrentMove: (context) => {},
+      }),
+    },
+    move: {
+      // Calculate possible valid destinations of piece
+      entry: assign({
+        cellsPossibleDestinationsCurrentMove: (context) => {},
+      }),
+      on: {
+        //
+        DESTINATION_CHOSEN: '',
+      },
+    },
+    turnFinished: {
+      // Transitive state to finish turn and to return to game machine
+    },
+  },
+})
+
+const differentGameMachine = Machine({
+  key: 'game',
+  initial: 'gameStart',
+  context: {
+    cellsPlacedPieces: [],
+    currentPlayer: 1,
+    unplacedPiecesPlayer1: [],
+    unplacedPiecesPlayer2: [],
+  },
+  states: {
+    gameStart: {
+      entry: assign((context) => {
+        // Select start player (propose random, animated in UI as coin flip or something)
+      }),
+    },
+    playerTurn: {
+      // Enter "sub"-machine to handle player turn
+    },
+    checkGameFinished: {
+      on: {
+        // Transient transition with conditionals to check whether game is over
+        '': [
+          {
+            target: 'gameOver',
+            cond: '' /* Conditional logic to check if queen is surrounded */,
+          },
+          { target: 'changeTurn' },
+        ],
+      },
+    },
+    changeTurn: {
+      // Transient state that simply changes player turn
+    },
+    gameOver: {},
+  },
+})
