c11a20fa8752090ce5265cc99201e4a627243bc0
Ward <WardMaes@users.noreply.github.com>
2020-11-03 08:53:49 +0100

    Create initial communication between sockets

--- machines/game.ts
+++ machines/game.ts
@@ -1,6 +1,7 @@
 import { Machine, assign } from 'xstate'
 
 import { HexCoord } from '../lib/hex'
+import { createRoom, joinRoom } from '../lib/p2p'
 import { turnMachine, TurnContext, TurnEvent } from './turn'
 
 const startCells: Cell[] = [
@@ -70,69 +71,88 @@ export interface Schema {
   }
 }
 
-export const gameMachine = Machine<Context, Schema, Event>({
-  id: 'game',
-  initial: 'initializing',
-  context: {
-    // Game-specific context
-    cellsOnBoard: startCells,
-    currentPlayer: 1,
-    unplacedInsectsPlayer1: [],
-    unplacedInsectsPlayer2: [],
-    // Turn-specific context
-    selectedPiece: null,
-    cellsPossibleDestinationsCurrentMove: [],
-    // 'piecesAllowedToBeMoved' and 'piecesAllowedToBePlaced' could maybe be merged into 1 property 'piecesAllowedToBePlayed'
-    piecesAllowedToBeMoved: [],
-    piecesAllowedToBePlaced: [],
-  },
-  states: {
-    initializing: {
-      entry: assign((_) => ({
-        // Select start player (propose random, animated in UI as coin flip or something)
-        // Set initial pieces for both players
-        unplacedInsectsPlayer1: [],
-        unplacedInsectsPlayer2: [],
-      })),
-      always: 'menu',
+export const gameMachine = Machine<Context, Schema, Event>(
+  {
+    id: 'game',
+    initial: 'initializing',
+    context: {
+      // Game-specific context
+      cellsOnBoard: startCells,
+      currentPlayer: 1,
+      unplacedInsectsPlayer1: [],
+      unplacedInsectsPlayer2: [],
+      // Turn-specific context
+      selectedPiece: null,
+      cellsPossibleDestinationsCurrentMove: [],
+      // 'piecesAllowedToBeMoved' and 'piecesAllowedToBePlaced' could maybe be merged into 1 property 'piecesAllowedToBePlayed'
+      piecesAllowedToBeMoved: [],
+      piecesAllowedToBePlaced: [],
     },
+    states: {
+      initializing: {
+        entry: assign((_) => ({
+          // Select start player (propose random, animated in UI as coin flip or something)
+          // Set initial pieces for both players
+          unplacedInsectsPlayer1: [],
+          unplacedInsectsPlayer2: [],
+        })),
+        always: 'menu',
+      },
 
-    menu: {
-      on: {
-        'GAME.JOIN': {
-          target: 'playing', // TODO: go to /rooms/[roomId]
-        },
-        'GAME.CREATE': {
-          target: 'playing', // TODO: create new room and go to /rooms/[roomId]
+      menu: {
+        on: {
+          'GAME.JOIN': {
+            target: 'playing', // TODO: go to /rooms/[roomId]
+            actions: ['joinRoom'],
+          },
+          'GAME.CREATE': {
+            target: 'playing', // TODO: go to /rooms/[roomId]
+            actions: ['createRoom'],
+          },
         },
       },
-    },
 
-    playing: {
-      ...turnMachine,
-      onDone: {
-        target: 'checkGameFinished',
-      },
-    },
-    checkGameFinished: {
-      // Transient transition with conditionals to check whether game is over
-      always: [
-        {
-          target: 'gameOver',
-          cond: '' /* Conditional logic to check if queen is surrounded */,
+      playing: {
+        ...turnMachine,
+        onDone: {
+          target: 'checkGameFinished',
         },
-        { target: 'alternating' },
-      ],
+      },
+      checkGameFinished: {
+        // Transient transition with conditionals to check whether game is over
+        always: [
+          {
+            target: 'gameOver',
+            cond: '' /* Conditional logic to check if queen is surrounded */,
+          },
+          { target: 'alternating' },
+        ],
+      },
+      alternating: {
+        // Transient state that simply changes player turn
+        always: {
+          actions: assign({
+            currentPlayer: (context) => (context.currentPlayer === 1 ? 2 : 1), // Alternate between players
+          }),
+          target: 'playing',
+        },
+      },
+      gameOver: {},
     },
-    alternating: {
-      // Transient state that simply changes player turn
-      always: {
-        actions: assign({
-          currentPlayer: (context) => (context.currentPlayer === 1 ? 2 : 1), // Alternate between players
-        }),
-        target: 'playing',
+  },
+  {
+    actions: {
+      createRoom: (context, event) => {
+        // console.log('actions.createRoom', context, event)
+        // create new room, with user as host
+        createRoom()
+      },
+      joinRoom: (context, event) => {
+        // console.log('actions.createRoom', context, event)
+        // create new room, with user as host
+        // @ts-ignore
+        joinRoom(event.code || 'abc')
       },
     },
-    gameOver: {},
-  },
-})
+  }
+)
---
+++ pages/rooms/[roomId].tsx
@@ -0,0 +1,30 @@
+import { GetServerSideProps, NextApiRequest, NextApiResponse } from 'next'
+import React from 'react'
+
+type RoomProps = {
+  id: string
+  roomId: string
+  initiator: boolean
+}
+
+const Room = ({ id, roomId, initiator }: RoomProps) => {
+  return (
+    <div>
+      id: {JSON.stringify(id)}
+      roomId: {JSON.stringify(roomId)}
+      initiator: {JSON.stringify(initiator)}
+    </div>
+  )
+}
+
+export default Room
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // Generate id based on roomId and return this
+
+  const roomId = context.params?.roomId
+  const id = 'sick' // TODO: generate
+  const initiator = true
+
+  return { props: { id, roomId, initiator } }
+}
