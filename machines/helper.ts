9f5fce591a237a23dedf735e7bd989f0d298a668
Toine <bestrandomnameever@gmail.com>
2020-11-20 01:15:47 +0100

    Extract context update with sync

--- machines/helper.ts
+++ machines/helper.ts
@@ -3,12 +3,28 @@ import { MachineOptions, EventObject } from 'xstate'
 export const mergeMachineOptions = <TContext, TEvent extends EventObject>(
   machineOptions: Partial<MachineOptions<TContext, TEvent>>[]
 ): Partial<MachineOptions<TContext, TEvent>> => {
-  let masterConfig = {}
+  let masterConfig: Partial<MachineOptions<TContext, TEvent>> = {}
   machineOptions.forEach((config) => {
-    masterConfig = {
-      ...masterConfig,
-      ...config,
-    }
+    ;(masterConfig.actions = {
+      ...masterConfig.actions,
+      ...config.actions,
+    }),
+      (masterConfig.activities = {
+        ...masterConfig.activities,
+        ...config.activities,
+      }),
+      (masterConfig.delays = {
+        ...masterConfig.delays,
+        ...config.delays,
+      }),
+      (masterConfig.guards = {
+        ...masterConfig.guards,
+        ...config.guards,
+      }),
+      (masterConfig.services = {
+        ...masterConfig.services,
+        ...config.services,
+      })
   })
   return masterConfig
 }
