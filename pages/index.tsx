24336b90df8b9e1a3d575f3af4e86e2eea94c27c
Ward <WardMaes@users.noreply.github.com>
2021-02-19 22:41:56 +0100

    Add twitch login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,46 @@
 import Head from 'next/head'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import { Menu } from '../components'
 
-export default function Home() {
+export default function Page() {
+  const [session, loading] = useSession()
+
+  const getRedemptions = async () => {
+    const res = await fetch(
+      'https://api.twitch.tv/helix/channel_points/custom_rewards/redemptions?reward_id=&broadcaster_id=' +
+        session.user_id,
+      {
+        headers: {
+          Authorization: 'Bearer ' + session.access_token,
+          'Client-Id': 'ex4mnguf70q3gj1xgre1xwzv4u24s8'
+        },
+      }
+    )
+    const answer = await res.json()
+    console.log('answer', answer)
+  }
+
+  const createReward = async() => {
+    const res = await fetch(
+      'https://api.twitch.tv/helix/channel_points/custom_rewards?broadcaster_id=' + session.user_id,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          title: 'test reward',
+          cost: 10
+        }),
+        headers: {
+          Authorization: 'Bearer ' + session.access_token,
+          'Client-Id': 'ex4mnguf70q3gj1xgre1xwzv4u24s8',
+          'Content-Type': 'application/json'
+        },
+      }
+    )
+    const answer = await res.json()
+    console.log('answer', answer)
+  }
+
   return (
     <>
       <Head>
@@ -12,7 +50,21 @@ export default function Home() {
 
       <div>
         <div className="my-32 p-8 bg-white shadow-md rounded">
-          <Menu />
+          {!session && (
+            <>
+              Not signed in <br />
+              <button onClick={() => signIn()}>Sign in</button>
+            </>
+          )}
+          {session && (
+            <>
+              Signed in as {JSON.stringify(session, null, 2)} <br />
+              <button onClick={() => signOut()}>Sign out</button>
+              <button onClick={() => getRedemptions()}>Test api</button>
+              <button onClick={() => createReward()}>Create</button>
+              <Menu />
+            </>
+          )}
         </div>
       </div>
     </>
